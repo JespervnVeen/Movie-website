1319927a310548a7fef3421a88882e1cee054c04
JespervnVeen <118971093+JespervnVeen@users.noreply.github.com>
2023-07-05 23:30:00 +0200

    g4t45t

--- cardsandrating.js
+++ cardsandrating.js
@@ -1,8 +1,8 @@
 function setup() {
     fetch("movies.json")
      .then(response => response.json())
-     .then(data => parse(data))
-};
+     .then(data => parse(data));
+}
 
 class Movie {
     constructor(image, name, year, director, rating) {
@@ -17,12 +17,19 @@ class Movie {
         const sectionMovie = document.getElementById("cards");
         const templateMovie = document.getElementById("template");
         const cloneMovie = templateMovie.content.cloneNode(true);
+        const templateButtons = cloneMovie.querySelectorAll(".templatebutton");
 
         cloneMovie.querySelector(".templateimage").src = this.image;
         cloneMovie.querySelector(".templatename").innerHTML = this.name;
         cloneMovie.querySelector(".templatedirector").innerHTML = this.director;
         cloneMovie.querySelector(".templateyear").innerHTML = this.year;
         cloneMovie.querySelector(".templaterating").innerHTML = this.rating;
+
+        templateButtons.forEach(button => {
+            button.addEventListener("click", () => {
+                    
+            });
+        });
 
         sectionMovie.appendChild(cloneMovie);
     }
@@ -33,17 +40,17 @@ function parse(data) {
     const sectionMovie = document.getElementById("cards");
     sectionMovie.innerHTML = "";
 
-    if(valueSort == "name") {
+    if (valueSort == "name") {
         data.sort((a, b) => a.name.localeCompare(b.name));
-    } else if(valueSort == "mostrecent") {
+    } else if (valueSort == "mostrecent") {
         data.sort((a, b) => new Date(b.year) - new Date(a.year));
-    } else if(valueSort == "oldest") {
+    } else if (valueSort == "oldest") {
         data.sort((a, b) => new Date(a.year) - new Date(b.year));
-    } else if(valueSort == "rating") {
+    } else if (valueSort == "rating") {
         data.sort((a, b) => b.rating - a.rating);
     }
 
-    for(movie of data) {
+    for (const movie of data) {
         let mov = new Movie(movie.image, movie.name, movie.year, movie.director, movie.rating);
         mov.html();
     }
@@ -52,7 +59,7 @@ function parse(data) {
 document.getElementById("sort").addEventListener("change", function() {
     fetch("movies.json")
      .then(response => response.json())
-     .then(data => parse(data))
+     .then(data => parse(data));
 });
 
 setup();
